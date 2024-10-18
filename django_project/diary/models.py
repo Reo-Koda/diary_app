63dc89367ffe2a1b2df8144ab8131af34eeac2cf
Reo-Koda <matoryoshika719@gmail.com>
2024-10-19 02:26:49 +0900

    add author function

--- django_project/diary/models.py
+++ django_project/diary/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from pathlib import Path
 import uuid
 
@@ -7,6 +8,7 @@ class Page(models.Model):
     title = models.CharField(max_length=120, verbose_name="タイトル")
     body = models.TextField(max_length=2000, verbose_name="本文")
     picture = models.ImageField(upload_to="diary/picture/", blank=True, null=True, verbose_name="画像")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, verbose_name="作成者")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="作成日時")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="更新日時")
 
