a4565e8c49fb6d9c44a85b3cc7508efb55e8f545
Reo-Koda <matoryoshika719@gmail.com>
2024-10-19 19:00:17 +0900

    Add home view for root URL

--- django_project/urls.py
+++ django_project/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("diary/", include("diary.urls")),
     path("accounts/", include("accounts.urls")),
     path("", include("django.contrib.auth.urls")),
+    path('', RedirectView.as_view(url='/diary/')),  # ルートURLをリダイレクト
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
